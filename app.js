0c98246857fd6dfaf825e3fecd226eb1baca5975
Luke Duncalfe <lduncalfe@eml.cc>
2018-04-30 22:02:26 +1200

    Removing commented out code, linting

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
 const http = require('http');
 const WebSocket = require('ws');
-const fs = require('fs');
-// const path = require('path');
-var ipc = require('node-ipc');
+const ipc = require('node-ipc');
 
-// ipc.config.id = 'nano-stream-ws';
+ipc.config.id = 'nanoStreamWebSockets';
 ipc.config.retry = 1500;
 
 const wsServer = new http.createServer();
 const wss = new WebSocket.Server({ server: wsServer });
 const wssPort = 8080;
 
-wss.on('connection', function connection(ws) {
-  console.log('Connection!');
-  // ws.on('message', function incoming(message) {
-  //   console.log('received: %s', message);
-  // });
-});
-
 ipc.connectTo(
   'nanoStream', () => {
-    // ipc.of.nanoStream.on(
-    //     'connect',
-    //     function(){
-    //         ipc.log('## connected to nanoStream ##'.rainbow, ipc.config.delay);
-    //         ipc.of.nanoStream.emit(
-    //             'message',  //any event or message type your server listens for
-    //             'hello'
-    //         )
-    //     }
-    // );
-    // ipc.of.nanoStream.on(
-    //   'disconnect',
-    //   function(){
-    //       ipc.log('disconnected from nanoStream'.notice);
-    //   }
-    // );
     ipc.of.nanoStream.on(
-      'payload',  // any event or message type your server listens for
+      'payload', // topic
       function(data){
-        // ipc.log('got a message from nanoStream : '.debug, data);
-        console.log('Sending websocket data to all clients');
-        // wss.emit(data);
+        console.debug('Sending websocket data to all clients');
         wss.clients.forEach(ws => ws.send(data));
       }
     );
@@ -50,8 +23,8 @@ ipc.connectTo(
 
 wsServer.listen(wssPort, (err) => {
   if (err) {
-    return console.log('something bad happened', err)
+    return console.error('something bad happened', err);
   }
 
-  console.log(`websocket server is listening on ${wssPort}`)
+  console.log(`websocket server is listening on ${wssPort}`);
 });
