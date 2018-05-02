e34663cfee55a4fb680a3de87098f11770a0b2af
Luke Duncalfe <lduncalfe@eml.cc>
2018-05-03 11:09:13 +1200

    Sending ping messages to connected clients.

--- app.js
+++ app.js
@@ -16,23 +16,44 @@ const CONF = {
 
 // Process any args passed in and overwrite defaults
 const args =  process.argv.slice(2);
-args.forEach((arg) => {
+args.forEach(arg => {
   const [key, value] = arg.split('=');
   CONF[key] = value;
 });
+
+// Set up ping messages to verify all clients are still connected
+function noop() {}
+
+function heartbeat() {
+  this.isAlive = true;
+}
+
+wss.on('connection', (ws, req) => {
+  ws.isAlive = true;
+  ws.on('pong', heartbeat); // Pong messages are automatically sent in response to ping messages as required by the spec.
+  console.debug(`Client connected from ${req.headers.origin}. Now ${wss.clients.size} connected clients.`);
+});
+
+setInterval(() => {
+  wss.clients.forEach(ws => {
+    if (ws.isAlive === false) return ws.terminate();
+
+    ws.isAlive = false;
+    ws.ping(noop);
+  });
+}, 30000);
 
 ipc.connectTo(
   'nanoStream', () => {
     ipc.of.nanoStream.on(
       'payload', // topic
-      function(data){
-        if (wss.clients.length > 0) {
-          console.debug('Sending websocket data to all clients');
+      (data) => {
+        if (wss.clients.size > 0) {
+          console.debug(`Sending websocket data to ${wss.clients.size} clients`);
           wss.clients.forEach(ws => ws.send(data));
         } else {
           console.debug('Payload received, but there are no connected clients');
         }
-
       }
     );
   }
